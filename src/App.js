8a069b48ca5b101d63e5797a6048dc1eb275fcba
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2019-01-09 20:33:02 -0800

    should did this sooner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./component/Form";
+import Meal from "./component/Meal";
 
 class App extends Component {
+  state = {
+    meals: []
+  };
+
+  addMeal = meal => {
+    this.setState({
+      meals: [meal, ...this.state.meals]
+    });
+  };
+
+  onDelete = id => {
+    this.setState({
+      meals: this.state.meals.filter(meal => meal.id !== id)
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="jumbotron">
+          <h2>Calorie Counter</h2>
+          <hr />
+          <Form onsubmit={this.addMeal} />
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Meal</th>
+                <th>Calories</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.meals.map(meal => (
+                <Meal
+                  key={meal.id}
+                  meal={meal}
+                  onDelete={() => this.onDelete(meal.id)}
+                />
+              ))}
+              <tr>
+                <td>Total:</td>
+                <td>
+                  <span role="img">🍎</span>
+                  {this.state.meals.reduce(
+                    (totalCalories, meal) => totalCalories + meal.calorie,
+                    0
+                  )}
+                </td>
+                <td />
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
